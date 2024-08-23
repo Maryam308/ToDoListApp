f45b4c19e87ce26c5b117cd992da69d954e84e4c
Maryam308 <mariam601me@gmail.com>
2024-08-23 16:42:37 +0300

    Basic Functionality coded

---
+++ ToDoList/Models/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  ToDoList
+//
+//  Created by Maryam Yousif on 23/08/2024.
+//
+
+import Foundation
---
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  ToDoList
+//
+//  Created by Maryam Yousif on 23/08/2024.
+//
+
+import Foundation
